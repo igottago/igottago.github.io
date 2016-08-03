bd6cd2b6666f81ecf94d262dc79000870f7fa1e4
immadsion <madison.cuellar6@gmail.com>
2016-08-03 12:26:17 -0400

    FINALLY DONE?????

--- listy.js
+++ listy.js
@@ -1,16 +1,16 @@
 
   var contentString1 = '<div id="content">'+
-                                      '<div id="siteNotice">'+
-                                      '</div>'+
-                                      '<h1 id="firstHeading" class="firstHeading">First Floor Restroom </h1>'+
-                                      '<div id="bodyContent">'+
-                                      '<p> Between Sunglasses spot & Perfumaniaz</p> '+
-                                      '<p> 401 Biscayne Blvd. Miami, FL 33132</p>'+
-                                      '<img src="handicap.png" height="20px">'+
-                                      '<img src="baby.png" height="20px">'+
-                                      '<p><img src="bath3.png"> <img src="bath3also.png"> </p>'+
-                                      '</div>'+
-                                      '</div>';
+                          '<div id="siteNotice">'+
+                          '</div>'+
+                          '<h1 id="firstHeading" class="firstHeading">First Floor Restroom </h1>'+
+                          '<div id="bodyContent">'+
+                          '<p> Between Sunglasses spot & Perfumaniaz</p> '+
+                          '<p> 401 Biscayne Blvd. Miami, FL 33132</p>'+
+                          '<img src="handicap.png" height="20px">'+
+                          '<img src="baby.png" height="20px">'+
+                          '<p><img src="bath3.png"> <img src="bath3also.png"> </p>'+
+                          '</div>'+
+                          '</div>';
 
 
     var contentString2 = '<div id="content">'+
@@ -173,6 +173,142 @@ var contentString11 = '<div id="content">'+
                            ''+
                            '</div>'+
                            '</div>';
-
 var stringlist = [contentString1,contentString2,contentString3,contentString4,contentString5,contentString6,contentString7,contentString8,contentString9,
 contentString10,contentString11];
+
+
+  var infowindow1 = new google.maps.InfoWindow({
+    content: stringlist[0]
+    });
+
+  var infowindow2 = new google.maps.InfoWindow({
+    content: stringlist[1]
+    });
+
+  var infowindow3 = new google.maps.InfoWindow({
+    content: stringlist[2]
+    });
+
+  var infowindow4 = new google.maps.InfoWindow({
+    content: stringlist[3]
+    });
+
+  var infowindow5 = new google.maps.InfoWindow({
+    content: stringlist[4]
+    });
+
+  var infowindow6 = new google.maps.InfoWindow({
+    content: stringlist[5]
+    });
+
+  var infowindow7 = new google.maps.InfoWindow({
+    content: stringlist[6]
+    });
+
+  var infowindow8 = new google.maps.InfoWindow({
+    content: stringlist[7]
+    });
+
+  var infowindow9 = new google.maps.InfoWindow({
+    content: stringlist[8]
+    });
+
+  var infowindow10 = new google.maps.InfoWindow({
+    content: stringlist[9]
+    });
+
+  var infowindow11 = new google.maps.InfoWindow({
+    content: stringlist[10]
+    });
+
+var infolist = [infowindow1,infowindow2,infowindow3,infowindow4,infowindow5,infowindow6,infowindow7,infowindow8,infowindow9,
+infowindow10,infowindow11];
+
+      function initMap() {
+        var myLatLng = {lat: 25.777044,  lng: -80.185453};
+        var map;
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 15
+          });
+
+      var iconBase = 'https://maps.google.com/mapfiles/kml/paddle/';
+
+      var icons = {
+        clean: {
+          icon: iconBase + 'grn-circle.png'
+        },
+        dirty: {
+          icon: iconBase + 'red-circle.png'
+        },
+
+      };
+
+      function addMarker(feature) {
+               var marker = new google.maps.Marker({
+                 position: feature.position,
+                 icon: icons[feature.type].icon,
+                 map: map
+               });
+               marker.addListener('click', function() {
+                 infolist[i].open(map, marker);
+               });
+             }
+
+             var features = [
+               {
+               position:  new google.maps.LatLng(25.777044, -80.185453),
+               type:'clean'
+              },
+               {
+                 position:  new google.maps.LatLng(25.779183, -80.186863),
+                 type:'dirty'
+                 //topbathroomleft
+               },
+               {
+                position:  new google.maps.LatLng(25.779216, -80.186821 ),
+                type: 'dirty'
+                //bottombathroomleft
+              },
+              {
+               position:  new google.maps.LatLng(25.773423, -80.186578),
+               type: 'dirty'
+               //parkbathroom
+             },
+             {
+               position:  new google.maps.LatLng(25.777099, -80.185427),
+               type: 'dirty'
+               //bathroom
+             },
+             {
+               position:  new google.maps.LatLng(25.777587, -80.190153),
+               type: 'clean'
+             },
+             {
+               position:  new google.maps.LatLng(25.7670007, -80.18926540000001),
+               type: 'clean'
+             },
+             {
+               position:  new google.maps.LatLng(25.764665, -80.192921),
+               type: 'clean'
+             },
+             {
+               position:  new google.maps.LatLng(25.765389, -80.19465),
+               type: 'clean'
+             },
+             {
+               position:  new google.maps.LatLng(25.77307, -80.189216),
+               type: 'dirty'
+               //pollotropical
+             },
+             {
+               position:  new google.maps.LatLng(25.772295, -80.18678),
+               type: 'dirty'
+               //hoteloutsidebathroom
+             },
+             ];
+
+             for (var i = 0, feature; feature = features[i]; i++) {
+          addMarker(feature);
+        }
+      }
